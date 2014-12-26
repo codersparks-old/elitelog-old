7821813aef9e66c08f490f05dac180e58de9fc09
codersparks <codersparks@gmail.com>
2014-12-26 17:05:53 +0000

    General Update

--- src/main/java/org/codersparks/elite/repository/CommodityDataRepository.java
+++ src/main/java/org/codersparks/elite/repository/CommodityDataRepository.java
@@ -10,24 +10,38 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(collectionResourceRel="commodities", path="commodities")
-public interface CommodityDataRepository extends MongoRepository<CommodityData, String>{
+@RepositoryRestResource(collectionResourceRel = "commodities", path = "commodities")
+public interface CommodityDataRepository extends
+		MongoRepository<CommodityData, String> {
 
-		@RestResource(rel="findByStation", path="byStation")
-		Page<CommodityData> findByStationIgnoreCaseOrderByCreatedDesc(@Param("station") String station, Pageable pageable);
-		
-		@RestResource(rel="bySystem", path="bySystem")
-		Page<CommodityData> findBySystemIgnoreCaseOrderByCreatedDesc(@Param("system") String system, Pageable pageable);
-		
-		@RestResource(rel="byName", path="byName")
-		Page<CommodityData> findByNameIgnoreCaseOrderByCreatedDesc(@Param("name") String name, Pageable pageable);
-		
-		@RestResource(rel="byDemandLevel", path="byDemandLevel")
-		Page<CommodityData> findByDemandLevelIn(@Param("level") Collection<String> demandLevel, Pageable pageable);
-		
-		@RestResource(rel="bySupplyLevel", path="bySupplyLevel")
-		Page<CommodityData> findBySupplyLevelIn(@Param("level") Collection<String> supplyLevel, Pageable pageable);
-		
-		
+	@RestResource(rel = "findByStation", path = "byStation")
+	Page<CommodityData> findByStationIgnoreCaseOrderByCreatedDesc(
+			@Param("station") String station, Pageable pageable);
+
+	@RestResource(rel = "bySystem", path = "bySystem")
+	Page<CommodityData> findBySystemIgnoreCaseOrderByCreatedDesc(
+			@Param("system") String system, Pageable pageable);
+
+	@RestResource(rel = "byName", path = "byName")
+	Page<CommodityData> findByNameIgnoreCaseOrderByCreatedDesc(
+			@Param("name") String name, Pageable pageable);
+
+	@RestResource(rel = "byDemandLevel", path = "byDemandLevel")
+	Page<CommodityData> findByDemandLevelIn(
+			@Param("level") Collection<String> demandLevel, Pageable pageable);
+
+	@RestResource(rel = "bySupplyLevel", path = "bySupplyLevel")
+	Page<CommodityData> findBySupplyLevelIn(
+			@Param("level") Collection<String> supplyLevel, Pageable pageable);
+
+	@RestResource(rel = "byNameAndStation", path = "byNameAndStation")
+	Page<CommodityData> findByNameIgnoreCaseAndStationInOrderByCreatedDesc(
+			@Param("name") String name,
+			@Param("station") Collection<String> station, Pageable pageable);
 	
+	@RestResource(rel = "byNameAndSystem", path = "byNameAndSystem")
+	Page<CommodityData> findByNameIgnoreCaseAndSystemInOrderByCreatedDesc(
+			@Param("name") String name,
+			@Param("system") Collection<String> system, Pageable pageable);
+
 }
