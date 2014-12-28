c9ae85b808e10a0fd9a32a245ed738d79b5e2c17
codersparks <codersparks@gmail.com>
2014-12-28 20:30:46 +0000

    Finished off commodity select box and added datatable to show prices

--- src/main/webapp/js/commodity-compare.js
+++ src/main/webapp/js/commodity-compare.js
@@ -1,3 +1,5 @@
+var commodityTable;
+
 $(document).ready(function() {
     
     var commodities = new Bloodhound({
@@ -5,36 +7,142 @@ $(document).ready(function() {
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         limit: 10,
         prefetch: {
-            // url points to a json file that contains an array of country names, see
-            // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
             url: '/api/commodities/distinct',
-            // the json file contains an array of strings, but the Bloodhound
-            // suggestion engine expects JavaScript objects so this converts all of
-            // those strings
-            filter: function(list) {
+            filter: function(response) {
                 //console.log(list['commodities'])
                 
-                return $.map(list['commodities'], function(country) { return { name: country }; });
-            }
+                return $.map(response['commodities'], function(country) { return { name: country }; });
+            },
+			ttl: 60000
         }
     });
-    
+	
     commodities.initialize();
-    
+
+	
     $('#commodities-typeahead').typeahead(null, {
        name: 'commodities',
        displayKey: 'name',
        source: commodities.ttAdapter()
     });
+        
+	// Give the user the option to refresh the data list
+	$('#commodity-refresh-btn').on('click', function() {
+		console.log("Refresh pressed");
+		commodities.clearPrefetchCache();
+		commodities.clearRemoteCache();
+		commodities.initialize(true);
+	})
+	
+	// Enable the tooltops on this page
+	 $('[data-toggle="tooltip"]').tooltip()
+	
     
-    $('.tt-dropdown').css('background-color','#fff');
-	$('.tt-hint').addClass('form-control');
+	
+	$('#commodity-show-btn').on('click', function() {
+		updateCommodityTable($('#commodities-typeahead').val());
+	});
     
-    
-    //console.log(commodities)
-    
-    // Convert the commodity table into a dataTable
-	$('#commodity-table').dataTable();
-    // Convert the 
-                
+	// Register the display of the help modal to the help div
+	$('#commodities-help').on('click', function() {
+		console.log("Help button pressed");
+		$('#helpModal').modal()
+	});
+	
+	
+	
+	
 });
+
+var commodityToDataTable = function(sSource, aoData, fnCallback) {
+	//extract name/value pairs into a simpler map for use later
+	var paramMap = {};
+	for (var i = 0; i < aoData.length; i++) {
+		paramMap[aoData[i].name] = aoData[i].value;
+	}
+
+	//page calculations
+	var pageSize = paramMap.iDisplayLength;
+	console.log("PageSize: " + pageSize)
+	var start = paramMap.iDisplayStart;
+	var pageNum = (start == 0) ? 1 : (start / pageSize) + 1; // pageNum is 1 based
+
+	// extract sort information
+	var sortCol = paramMap.iSortCol_0;
+	var sortDir = paramMap.sSortDir_0;
+	var sortName = paramMap['mDataProp_' + sortCol];
+
+	//create new json structure for parameters for REST request
+	var restParams = new Array();
+	restParams.push({
+		"name" : "size",
+		"value" : pageSize
+	});
+	restParams.push({
+		"name" : "page",
+		"value" : pageNum
+	});
+	restParams.push({
+		"name" : "sort",
+		"value" : sortName + "," + sortDir
+	});
+	
+	console.log("Sort Name: " + sortName)
+	// TODO: Add filter "stuff" here
+	var url = sSource;
+	
+	console.log("Commodities Datatable URL: " + url)
+	console.log("Commodities Datatable Rest Params: " + JSON.stringify(restParams))
+	
+	// finally, make the request
+	$.ajax({
+		"dataType" : "json",
+		"type" : "GET",
+		"url" : url,
+		"data" : restParams,
+		"success" : function(data) {
+			console.log("Commodities datatables ajax returned: " + JSON.stringify(data))
+			data.iTotalRecords = data.page.totalElements;
+			data.iTotalDisplayRecords = data.page.totalElements;
+			fnCallback(data);
+		}
+	});
+
+};
+
+function updateCommodityTable(commodity) {
+	
+	console.log(commodityTable)
+	
+	if (commodityTable != undefined) {
+		commodityTable.fnDestroy();
+	}
+	
+	var url;
+	
+	if (commodity) {
+		url = "/api/commodities/search/byName?name=" + commodity;
+	} else {
+		url = "/api/commodities/";
+	}
+	
+	console.log("Updating table for: " + commodity);
+	// Convert the commodity table into a dataTable
+	commodityTable = $('#commodity-table').dataTable({
+		"sDom" : 'R<"row"<"col-sm-6"l><"col-sm-6"C>><"row"<"col-sm-12"t>><"row"<"col-sm-6"i><"col-sm-6"p>>',
+		"sAjaxSource" : url,
+		"sAjaxDataProp" : "_embedded.commodities",
+		"aoColumns" : [
+			{mDataProp:'station'},
+			{mDataProp:'system'},
+			{mDataProp:'name'},
+			{mDataProp:'buy'},
+			{mDataProp:'sell'},
+			{mDataProp:'supplyLevel'},
+			{mDataProp:'demandLevel'},
+			{mDataProp:'created'}
+		],
+		"bServerSide" : true,
+		"fnServerData" : commodityToDataTable
+	});
+}
